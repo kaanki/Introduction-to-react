f08239fbb0816a766cf0bd698796fb56501835e7
kaan keles <kaan.keles@mail.com>
2023-04-29 19:34:51 +0300

    react component props

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 
 // const First = () => {
@@ -13,48 +14,36 @@ import React from "react";
 // const Third = () => {
 //   return <p>bu benim mesajım</p>;
 // };
-const firstFood = {
-  storeName: "Lets Salad",
-  foodName: "Salad",
-  img: "https://images.deliveryhero.io/image/fd-tr/LH/zw8k-hero.jpg?width=200&height=200&quality=45",
-};
-const secondFood = {
-  storeName: "KaraFırın",
-  foodName: "Ekmek",
-  img: "https://images.deliveryhero.io/image/fd-tr/LH/uvwp-hero.jpg?width=200&height=200&quality=45",
-};
+const foods = [
+  {
+    storeName: "KaraFırın",
+    foodName: "Ekmek",
+    img: "https://images.deliveryhero.io/image/fd-tr/LH/zw8k-hero.jpg?width=200&height=200&quality=45",
+  },
+  {
+    storeName: "Lets Salad",
+    foodName: "Salad",
+    img: "https://images.deliveryhero.io/image/fd-tr/LH/h9q5-hero.jpg?width=200&height=200&quality=45",
+  },
+];
 
 function FoodOrder() {
   return (
     <section className="foodOrder">
-      <Food
-        storeName={firstFood.storeName}
-        foodName={firstFood.foodName}
-        img={firstFood.img}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque vitae
-          doloribus nesciunt doloremque magnam, aut perferendis ducimus nisi
-          reprehenderit velit, quos quae quod eos pariatur.
-        </p>
-        <button>Tıklayınız</button>
-      </Food>
-      <Food
-        storeName={secondFood.storeName}
-        foodName={secondFood.foodName}
-        img={secondFood.img}
-      />
+      {foods.map((food) => {
+        const { storeName, foodName, img } = food;
+        return <Food img={img} storeName={storeName} foodName={foodName} />;
+      })}
     </section>
   );
 }
 
-const Food = ({ img, storeName, foodName, children }) => {
+const Food = ({ img, storeName, foodName }) => {
   return (
     <article className="food">
       <img src={img} alt={foodName} />
       <h2>{foodName}</h2>
       <h4>{storeName} </h4>
-      {children}
     </article>
   );
 };
