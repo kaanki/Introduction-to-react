5e63afd8317cd74b81c7f636f3e34a614d4c1dff
kaan keles <kaan.keles@mail.com>
2023-04-29 18:56:35 +0300

    children

--- src/app/page.js
+++ src/app/page.js
@@ -13,42 +13,50 @@ import React from "react";
 // const Third = () => {
 //   return <p>bu benim mesajım</p>;
 // };
+const firstFood = {
+  storeName: "Lets Salad",
+  foodName: "Salad",
+  img: "https://images.deliveryhero.io/image/fd-tr/LH/zw8k-hero.jpg?width=200&height=200&quality=45",
+};
+const secondFood = {
+  storeName: "KaraFırın",
+  foodName: "Ekmek",
+  img: "https://images.deliveryhero.io/image/fd-tr/LH/uvwp-hero.jpg?width=200&height=200&quality=45",
+};
 
-const FoodOrder = () => {
+function FoodOrder() {
   return (
     <section className="foodOrder">
-      <Food />
-      <Food />
+      <Food
+        storeName={firstFood.storeName}
+        foodName={firstFood.foodName}
+        img={firstFood.img}
+      >
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque vitae
+          doloribus nesciunt doloremque magnam, aut perferendis ducimus nisi
+          reprehenderit velit, quos quae quod eos pariatur.
+        </p>
+        <button>Tıklayınız</button>
+      </Food>
+      <Food
+        storeName={secondFood.storeName}
+        foodName={secondFood.foodName}
+        img={secondFood.img}
+      />
     </section>
   );
-};
+}
 
-const Food = () => {
+const Food = ({ img, storeName, foodName, children }) => {
   return (
     <article className="food">
-      <Image />
-      <Order />
-      <Store />
+      <img src={img} alt={foodName} />
+      <h2>{foodName}</h2>
+      <h4>{storeName} </h4>
+      {children}
     </article>
   );
-};
-
-const Image = () => {
-  return (
-    <img
-      src="https://images.deliveryhero.io/image/fd-tr/LH/h9q5-hero.jpg?width=300&height=200&quality=45"
-      alt="ev yemekleri"
-    />
-  );
-};
-const Order = () => <h2>Food Order</h2>;
-const Store = () => {
-  const inlineHeadingStyles = {
-    color: "#617d98",
-    fontSize: "0.75rem",
-    marginTop: "0.5rem",
-  };
-  return <h4 style={inlineHeadingStyles}>Let's Salad</h4>;
 };
 
 export default FoodOrder;
